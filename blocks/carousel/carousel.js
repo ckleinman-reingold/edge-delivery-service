dac15f8382036b3b48c43ddb414d7fe9066df46b
Corinne Kleinman <ckleinman@H0JMJYNHXL.local>
2025-08-25 15:50:08 -0400

    carousel css

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,17 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default function decorate(block) {
-  // Your JS logic to decorate the block,
-  // given the block DOM element
+  /* change to div, div */
+  const div = document.createElement('div');
+  [...block.children].forEach((row) => {
+    const div = document.createElement('div');
+    while (row.firstElementChild) div.append(row.firstElementChild);
+    [...div.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'carousel-image';
+      else div.className = 'carousel-body';
+    });
+    div.append(div);
+  });
+  div.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.replaceChildren(div);
 }
